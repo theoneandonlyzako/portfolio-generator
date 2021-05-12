800518f7e2d36bac350a7102b4bcec512b87a556
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-12 02:35:56 -0700

    added finihsed site stuff

--- src/page-template.js
+++ src/page-template.js
@@ -86,7 +86,8 @@ module.exports = templateData => {
       </div>
     </header>
     <main class="container my-5">
-
+    ${generateAbout(about)}
+    ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
