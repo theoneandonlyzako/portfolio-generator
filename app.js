c687a634b7b386eac1cc7ad96d1a94e1cb8e05bd
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-11 22:58:37 -0700

    finished the questions part

--- app.js
+++ app.js
@@ -42,18 +42,23 @@ const promptUser = () => {
         }
     },
     {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself (required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('please enter information about yourself!');
-                return false;                
-            }
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } else {
+            return false;
+          }
         }
-    },
+      },
     {
         type: 'input',
         name: 'link',
@@ -66,6 +71,12 @@ const promptUser = () => {
                 return false;                
             }
         }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
       }
 ]);
 };
