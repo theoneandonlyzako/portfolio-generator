6f27e04035123ec3ba0405288a08f4fdb911b8a6
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-11 21:40:57 -0700

    added question functionality

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
   
-fs.writeFile('./index.html', generatePage(name, github), err => {
-if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+// if (err) throw new Error(err);
 
-console.log('portfolio complete! checkout index.html to see the output!');
-});
+// console.log('portfolio complete! checkout index.html to see the output!');
+// });
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your Name?'
+    }
+])
+.then(answers => console.log(answers));
+
